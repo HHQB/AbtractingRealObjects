c87635ce76928b68f82582832d979ed3e88107b9
AidenM <145621751+HHQB@users.noreply.github.com>
2025-11-19 10:31:20 -0800

    Add files via upload

--- App.java
+++ App.java
@@ -1,6 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
         Plane plane1 = new Plane("Boeing 747", new Passengers(500));
-        System.out.println("Plane One: " + plane1);
+        Plane plane2 = new Plane("Airbus A380", new Passengers(300), 600);
+        System.out.println("Plane 1: " + plane1);
+        System.out.println("Plane 2: " + plane2);
+        plane1.setAltitude(10000);
+        plane1.unloadPassengers();
+        plane2.loadPassengers(250);
+        System.out.println(plane2.getPassengers());
         }
     }
